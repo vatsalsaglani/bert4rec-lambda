74811d86d6eefc1583ade927a38300c3ab64beb1
vatsalsaglani <saglanivatsal@gmail.com>
2022-08-13 16:30:17 +0530

    api to recommend given a sequence

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,114 @@
+import os
 import json
+from msilib import sequence
+import traceback
+from fastapi import FastAPI
+from pydantic import BaseModel
+from mangum import Mangum
+from typing import Union, List, Dict
+
+from predict import Recommender
+
+model_available = False
+if os.path.exists("/mnt/efs/bert4rec-state-dict.pth"):
+    model_available = True
+
+if model_available:
+    rec_obj = Recommender()
+
+app = FastAPI()
+
+
+class RecommendRequestModel(BaseModel):
+    id: Union[str, int, None] = None
+    sequence: List[Union[str, int]]
+    history: Union[int, None] = 10
+
+
+
+@app.get("/")
+def parent_get():
+    return "Hello World"
+
+
+@app.post("/api/recommend")
+def api_recommend(payload: RecommendRequestModel):
+    if model_available:
+        try:
+            id = payload.id
+            sequence = payload.sequence
+            sequence = [int(s) for s in sequence]
+            history = payload.history
+            seq, hist, rec = rec_obj.recommend(sequence=sequence, num_recs=history)
+            return dict(
+                ok = True,
+                recommendations = rec,
+                seq = seq,
+                hist = hist,
+                id = id,
+                message = dict(
+                    traceback = "",
+                    message = "success",
+                    info = ""
+                )
+            )
+        except Exception as e:
+            print(f'GOT EXCEPTION: {str(e)}')
+            print(f'TRACEBACK: {traceback.format_exc()}')
+            return dict(
+                ok = False,
+                message = dict(
+                    traceback = f"""
+                    TRACEBACK: {traceback.format_exc()}
+                    ERROR: {str(e)}
+                    """,
+                    message = "failure",
+                    info = "Error recommending"
+                )
+            )
+    else:
+        return dict(ok = False, message = {"traceback": "", "message": "Model not available in efs", "info": f"Things in EFS: {os.listdir('/mnt/efs')}"})
+
+
+lambda_handler = Mangum(app, lifespan="off")
 
 # import requests
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+# def lambda_handler(event, context):
+#     """Sample pure Lambda function
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+#     Parameters
+#     ----------
+#     event: dict, required
+#         API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+#         Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+#     context: object, required
+#         Lambda Context runtime methods and attributes
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+#         Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
+#     Returns
+#     ------
+#     API Gateway Lambda Proxy Output Format: dict
 
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
+#         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+#     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
+#     # try:
+#     #     ip = requests.get("http://checkip.amazonaws.com/")
+#     # except requests.RequestException as e:
+#     #     # Send some context about this error to Lambda Logs
+#     #     print(e)
 
-    #     raise e
+#     #     raise e
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+#     return {
+#         "statusCode": 200,
+#         "body": json.dumps({
+#             "message": "hello world",
+#             # "location": ip.text.replace("\n", "")
+#         }),
+#     }
